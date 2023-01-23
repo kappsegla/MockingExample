f858f2f3dbe8404d84ed89520c9982db89b1235a
Christofer Nyström <christofer.nystrom@iths.se>
2023-01-23 11:08:24 +0100

    Adds unfinished EmployeRepositoryInMemory class
    Class is TODO for later.

---
+++ src/main/java/com/example/EmployeeRepositoryInMemory.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.List;
+
+//TODO
+public class EmployeeRepositoryInMemory implements EmployeeRepository {
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee save(Employee e) {
+        return null;
+    }
+
+
+}
