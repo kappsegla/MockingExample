ff784f563a5ea5b20ac18487039a8804383144da
Christofer Nyström <christofer.nystrom@iths.se>
2023-01-28 17:41:25 +0100

    Adds empty Classes

---
+++ src/main/java/BowlingGame/Frame.java
@@ -0,0 +1,4 @@
+package BowlingGame;
+
+public class Frame {
+}
--- src/test/java/com/example/EmployeeRepositoryInMemoryTest.java
+++ src/test/java/com/example/EmployeeRepositoryInMemoryTest.java
@@ -16,22 +16,36 @@ class EmployeeRepositoryInMemoryTest {
     @BeforeEach
     void setUp() {
         employeeRepositoryInMemory = new EmployeeRepositoryInMemory(employees = new ArrayList<>());
-        employees.add(new Employee("001", 40000));
+        employeeRepositoryInMemory.save(new Employee("001", 40000));
         employees.add(new Employee("002", 45000));
-        employeeRepositoryInMemory.save(new Employee("003", 50000));
+        employees.add(new Employee("003", 50000));
+
     }
 
     @Test
-    void findAllShouldReturnThree() {
+    void findAllShouldHaveCorrectSize() {
         int size = employeeRepositoryInMemory.findAll().size();
         assertThat(size).isEqualTo(3);
+    }
+
+    @Test
+    void saveShouldReplaceEmployeeIfEmployeeIdExists() {
+        employeeRepositoryInMemory.save(new Employee("003", 60000));
+        int size = employeeRepositoryInMemory.findAll().size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @Test
+    void saveShouldAddEmployeeIfEmployeeIdDontExists() {
+        employeeRepositoryInMemory.save(new Employee("004", 50000));
+        int size = employeeRepositoryInMemory.findAll().size();
+        assertThat(size).isEqualTo(4);
     }
 
     @Test
     void employeeShouldBeReplacedByEmployeeWithSameIdButHigherSalary(){
 
         employeeRepositoryInMemory.save(new Employee("003",60000));
-
         var newSalary = Objects.requireNonNull(employeeRepositoryInMemory.findAll().stream()
                         .filter(e -> e.getId().equals("003"))
                         .findFirst()
