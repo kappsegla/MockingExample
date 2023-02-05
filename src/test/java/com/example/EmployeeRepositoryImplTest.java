f14e38443fa068131203d5ace82b7f634b630e86
Osedumme Steven <stephen.osedumme@iths.se>
2023-02-05 16:28:50 +0100

    BDD Testing

--- src/test/java/com/example/EmployeeRepositoryImplTest.java
+++ src/test/java/com/example/EmployeeRepositoryImplTest.java
@@ -2,21 +2,47 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EmployeeRepositoryImplTest {
 
-    @Test
-    void findAll() {
-        Employee employee = new Employee("101",12550.50);
-    }
+    EmployeeRepositoryImpl employeeRepository = new EmployeeRepositoryImpl(List.of
+            (new Employee("1", 12550.50), new Employee("2", 13550.50), new Employee("3", 14550.50)));
 
     @Test
-    void save() {
+    void findAllEmployeeOnTheList() {
+        var result = employeeRepository.findAll().size();
+        assertEquals(3, result );
+    }
+    @Test
+    void findEmployeeOnTheListBySalary() {
+        var result = employeeRepository.employees.get(0).getSalary();
+        assertEquals(12550.50,result);
+    }
+    @Test
+    void findEmployeeOnTheListById() {
+        var result = employeeRepository.employees.get(0).getId();
+        assertEquals("1",result);
+    }
+
+
+    @Test
+    void savedNewEmployeeToTheList() {
+        Employee employee = new Employee("3", 15550.50);
+        employeeRepository.save(employee);
+        var result = employeeRepository.findAll().size();
+
+        assertEquals(3, result );
     }
 
     @Test
     void clearDatabase() {
+        employeeRepository.clearDatabase();
+        var result = employeeRepository.findAll().size();
+
+        assertEquals(0 , result);
 
     }
 }
