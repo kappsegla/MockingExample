24603c1497b7503c10f428c3f241594ad59b6c15
Linus Andreasson <lmmandreasson@gmail.com>
2024-04-03 13:42:15 +0200

    EmployeeTest added

---
+++ src/test/java/com/example/EmployeeTest.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class EmployeeTest {
+
+
+    private Employee employee;
+
+    @BeforeEach
+    void init(){
+        employee = new Employee("1337", 30000);
+    }
+
+    @Test
+    @DisplayName("GetID returns ID 1337 when ID is 1337")
+    void getIDreturnsID1337(){
+        boolean result = employee.getId().equals("1337");
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("GetSalary returns 30000 when salary is 30000")
+    void getSalaryReturns30000WhenSalaryIs30000(){
+        boolean result = employee.getSalary()==30000;
+        assertTrue(result);
+    }
+
+    @Test
+    @DisplayName("IsPaid returns false if paid not set")
+    void isPaidReturnsTrueIfPaidNotSet(){
+        assertFalse(employee.isPaid());
+    }
+
+    @Test
+    @DisplayName("ID is correct after setID")
+    void IDIsCorrectAfterSetID (){
+        employee.setId("13");
+        assertTrue(employee.getId().equals("13"));
+    }
+    @Test
+    @DisplayName("Salary is correct after setSalary")
+    void salaryIsCorrectAfterSetSalary (){
+        employee.setSalary(333000);
+        assertTrue(employee.getSalary()==333000);
+    }
+
+    @Test
+    @DisplayName("isPaid is true after setPaid to true")
+    void isPaidIsTrueAfterSetPaidToTrue (){
+        employee.setPaid(true);
+        assertTrue(employee.isPaid());
+    }
+
+}
