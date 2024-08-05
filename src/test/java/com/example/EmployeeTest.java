ef21882adfc470c3a8eb1e5277d355f93a62ca03
demppas <dennis.staffansson@iths.se>
2024-08-05 23:02:15 +0200

    added all testmethods for employee class

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -30,4 +30,21 @@ class EmployeeTest {
         employee.setSalary(20000);
         assertEquals(20000, employee.getSalary());
     }
+    @Test
+    void TestIsPaid(){
+        var employee = new Employee("1", 10000);
+        assertFalse(employee.isPaid());
+    }
+    @Test
+    void TestSetPaid(){
+        var employee = new Employee("1", 10000);
+        employee.setPaid(true);
+        assertTrue(employee.isPaid());
+    }
+    @Test
+    void TestReturnStringWithIdAndSalary(){
+        var employee = new Employee("1", 10000);
+        String expected = "Employee [id=1, salary=10000.0]";
+        assertEquals(expected, employee.toString());
+    }
 }
