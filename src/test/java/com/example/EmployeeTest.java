e9b32b1489f65f6bc7380e8d0c50954a6645448e
demppas <dennis.staffansson@iths.se>
2024-08-05 22:46:46 +0200

    added testmethod for setId and setSalary

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -17,5 +17,17 @@ class EmployeeTest {
         var employee = new Employee("1", 10000);
         assertEquals(10000, employee.getSalary());
     }
+    @Test
+    void TestSetId(){
+        var employee = new Employee("1", 10000);
+        employee.setId("2");
+        assertEquals("2", employee.getId());
+    }
 
+    @Test
+    void TestSetSalary(){
+        var employee = new Employee("1", 10000);
+        employee.setSalary(20000);
+        assertEquals(20000, employee.getSalary());
+    }
 }
