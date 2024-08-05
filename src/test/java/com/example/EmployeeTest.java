ea9e2358fb28d85f49a0a0a83c8b621eb619a10c
demppas <dennis.staffansson@iths.se>
2024-08-05 22:14:14 +0200

    created test for getId and getSalary

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
+
+    @Test
+    void TestGetId(){
+        var employee = new Employee("1", 10000);
+        assertEquals("1", employee.getId());
+    }
+
+    @Test
+    void TestGetSalary(){
+        var employee = new Employee("1", 10000);
+        assertEquals(10000, employee.getSalary());
+    }
 
 }
