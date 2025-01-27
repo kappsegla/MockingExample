cb8bd1826a22bfacc8d73c9b3882f614d1921808
efpcode <efpjobhunt@gmail.com>
2025-01-27 17:16:06 +0100

    Adds test cases as a FixMe list

--- src/test/java/com/example/exercise2/ShoppingCartTest.java
+++ src/test/java/com/example/exercise2/ShoppingCartTest.java
@@ -1,4 +1,11 @@
 package com.example.exercise2;
 
 public class ShoppingCartTest {
+    //FIXME Add the following test cases for ShoppingCartTest:
+    //  - Add items
+    //  - Delete items
+    //  - Calculate total sum for all items
+    //  - Apply sales discount
+    //  - Mange/handle item stock updates
+
 }
