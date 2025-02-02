4d40bf64d598464a42571cedb6c6e95644b3079d
Minhya <minhy96th@gmail.com>
2025-02-02 23:44:02 +0100

    påbörjar shoppingcart

--- src/main/java/com/example/ShoppingCart.java
+++ src/main/java/com/example/ShoppingCart.java
@@ -1,4 +1,9 @@
 package com.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShoppingCart {
+    private final Map<String, CartItem> items = new HashMap<>();
+    private double discount = 0.0;
 }
--- src/test/java/com/example/ShoppingCartTest.java
+++ src/test/java/com/example/ShoppingCartTest.java
@@ -1,6 +1,14 @@
 package com.example;
 
+import org.junit.jupiter.api.BeforeEach;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ShoppingCartTest {
+    private ShoppingCart shoppingCart;
+
+    @BeforeEach
+    void setUp() {
+        shoppingCart = new ShoppingCart();
+    }
   
 }
